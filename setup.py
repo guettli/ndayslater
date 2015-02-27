6bc6282495706f71402ba0c22cd725afdbff8f55
Thomas Guettler <guettli.git@thomas-guettler.de>
2015-02-27 23:42:51 +0100

    - initial commit: setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ndayslater',
+      version='1.0',
+      description='Shedule mails for resubmission N days later',
+      author='Thomas Guettler',
+      author_email='info.ndayslater@thomas-guettler.de',
+      url='https://github.com/guettli/ndayslater',
+      packages=['ndayslater'],
+     )
