52d1a45bab721d7a798baf286ef775bfd1a359ab
Thomas Guettler <guettli.git@thomas-guettler.de>
2015-02-28 19:21:45 +0100

    read host, user pwd from config file. directories now have name "day01" not "d01"

--- ndayslater/ndayslater.py
+++ ndayslater/ndayslater.py
@@ -1,10 +1,11 @@
 import configargparse
 import imapclient
 import datetime
+import re
 
 
 def main():
-    config_parser = configargparse.ArgParser(default_config_files=['~/.config/ndayslater'])
+    config_parser = configargparse.ArgParser(default_config_files=['~/.config/ndayslater/ndayslater.conf'])
     config_parser.add('-c', '--my-config', required=False, is_config_file=True, help='config file path')
     config_parser.add('-o', '--host', required=True)
     config_parser.add('-u', '--user', required=True)
@@ -16,12 +17,13 @@ def main():
 
 class NDaysLaterIMAPClient(imapclient.IMAPClient):
     INBOX='INBOX'
+
     def __init__(self, args):
         self.args=args
         super(NDaysLaterIMAPClient, self).__init__(args.host, use_uid=True, ssl=True)
 
     def get_day_folder(self, day_as_int):
-        return '%s/d%02d' % (self.args.name_of_base_folder, day_as_int)
+        return '%s/day%02d' % (self.args.name_of_base_folder, day_as_int)
 
     def create_days_folders(self):
         self.get_or_create_folder(self.args.name_of_base_folder)
@@ -49,8 +51,23 @@ class NDaysLaterIMAPClient(imapclient.IMAPClient):
         self.add_flags(messages, [imapclient.DELETED])
         self.expunge()
 
+    def update_to_layout_version_1(self):
+        # update to new layout
+        for flags, delim, folder_name in self.list_folders(self.args.name_of_base_folder):
+            if not '/' in folder_name:
+                continue
+            print(folder_name)
+            new_name=re.sub(r'/d', '/day', folder_name)
+            #assert 0, new_name
+            self.rename_folder(folder_name, new_name)
+            self.subscribe_folder(new_name)
+
 def run(args):
     server = NDaysLaterIMAPClient(args)
     server.login(args.user, args.password)
+
     server.create_days_folders()
     server.move_today_to_inbox()
+
+if __name__=='__main__':
+    main()
