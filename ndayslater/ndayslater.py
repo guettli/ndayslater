aa524110ec8cfd082d255557d3b3821ce3e3459a
Thomas Guettler <guettli.git@thomas-guettler.de>
2015-03-19 09:48:55 +0100

    option --verbose, more logging, started "storing last run in mail message on server".

--- ndayslater/ndayslater.py
+++ ndayslater/ndayslater.py
@@ -1,7 +1,15 @@
+import email
+from email.mime.text import MIMEText
+import json
 import configargparse
 import imapclient
 import datetime
 import re
+import asjson as json
+
+import logging
+logger=logging.getLogger(__name__)
+
 
 
 def main():
@@ -10,13 +18,61 @@ def main():
     config_parser.add('-o', '--host', required=True)
     config_parser.add('-u', '--user', required=True)
     config_parser.add('-p', '--password', required=True)
+    config_parser.add('-v', '--verbose', action='store_true')
     config_parser.add('--name-of-base-folder', default='ndayslater')
 
     args = config_parser.parse_args()
+    if args.verbose:
+        loglevel=logging.DEBUG
+    else:
+        loglevel=logging.INFO
+
+    logging.basicConfig(level=loglevel)
+    logger.debug('start')
+
     run(args)
+
+class LastRun(object):
+
+    datetime_of_last_run=None
+    attributes=set(['datetime_of_last_run'])
+
+    def __init__(self, mail_rfc822):
+        msg = email.message_from_string(mail_rfc822)
+        for part in msg.walk():
+            payload=part.get_payload(decode=True)
+            data=json.loads(payload)
+            self.load_data(data)
+            break
+        if not self.datetime_of_last_run:
+            raise ValueError('failed to initialize LastRun')
+
+    def load_data(self, data):
+        seen=set()
+        for key, value in data.items():
+            if key not in self.attributes:
+                logger.warn('unsupported key in data? %r %r' % (key, value))
+                continue
+            seen.add(key)
+            setattr(self, key, value)
+        if not seen==self.attributes:
+            logger.warn('missing attributes during load_data(): attributes=%s seen=%s' % (self.attributes, seen))
+
+    @classmethod
+    def create_from_msg_id(cls, server, msgid):
+        folder=server.args.name_of_base_folder
+        server.select_folder(folder)
+        mail_rfc822=server.fetch([msgid], ['RFC822'])
+        return cls(mail_rfc822)
+
+    @classmethod
+    def create_from_scratch(cls, server):
+        msg = MIMEText(json.dumps(dict(datetime_of_last_run=datetime.datetime.now())))
+        return cls(msg.as_string())
 
 class NDaysLaterIMAPClient(imapclient.IMAPClient):
     INBOX='INBOX'
+    subject_of_status_mail='status_of_last_ndayslater_run'
 
     def __init__(self, args):
         self.args=args
@@ -35,6 +91,7 @@ class NDaysLaterIMAPClient(imapclient.IMAPClient):
         try:
             return self.select_folder(folder_name)
         except self.Error, exc:
+            logger.debug(('creating folder %s' % folder_name))
             self.create_folder(folder_name)
             return self.select_folder(folder_name)
 
@@ -43,10 +100,12 @@ class NDaysLaterIMAPClient(imapclient.IMAPClient):
         return self.move_date_to_inbox(today)
 
     def move_date_to_inbox(self, date):
-        self.get_or_create_folder(self.get_day_folder(date.day))
-        self.move(self.search(['NOT DELETED']), self.INBOX)
+        day_folder=self.get_day_folder(date.day)
+        self.get_or_create_folder(day_folder)
+        self.move(self.search(['NOT DELETED']), self.INBOX, day_folder)
 
-    def move(self, messages, folder):
+    def move(self, messages, folder, from_message='?'):
+        logger.debug('moving %s messages from %s to %s' % (len(messages), from_message, folder))
         self.copy(messages, folder)
         self.add_flags(messages, [imapclient.DELETED])
         self.expunge()
@@ -62,12 +121,51 @@ class NDaysLaterIMAPClient(imapclient.IMAPClient):
             self.rename_folder(folder_name, new_name)
             self.subscribe_folder(new_name)
 
+    def get_datetime_of_last_run_or_today(self):
+        datetime_of_last_run=self.get_datetime_of_last_run_or_none()
+        if datetime_of_last_run:
+            return datetime_of_last_run
+
+
+    def get_last_run(self):
+        last_run=self.get_last_run_or_none()
+        if last_run:
+            return last_run
+        return LastRun.create_from_scratch(self)
+
+    def get_last_run_or_none(self):
+        folder=self.args.name_of_base_folder
+        self.select_folder(folder)
+        try:
+            msg_id=self.search(['SUBJECT %s' % self.subject_of_status_mail])[0]
+        except IndexError:
+            logger.info('No mail with subject %r found. First run?' % self.subject_of_status_mail)
+            return
+        return LastRun.create_from_msg_id(self, msg_id)
+
 def run(args):
     server = NDaysLaterIMAPClient(args)
     server.login(args.user, args.password)
 
     server.create_days_folders()
+    last_run=server.get_last_run()
+    now=datetime.datetime.now()
+    if last_run.datetime_of_last_run.date()>now.date():
+        logger.warn('last run in the future? last_run=%s now=%s' % (last_run, now))
+        last_run.datetime_of_last_run=now
+    delta_since_last_run = now - last_run.datetime_of_last_run
+    if delta_since_last_run > datetime.timedelta(days=31):
+        logger.warn('last run is very old. I ignore this old value: last_run=%s now=%s' % (last_run, now))
+        last_run.datetime_of_last_run=now
+    day_of_step=last_run.datetime_of_last_run.date()
+    while day_of_step<now.date():
+        server.move_date_to_inbox(day_of_step)
+        day_of_step=day_of_step+datetime.timedelta(days=1)
+
     server.move_today_to_inbox()
+    logger.debug('done')
+
 
 if __name__=='__main__':
     main()
+
