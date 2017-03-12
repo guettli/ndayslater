f639eef5aa7a9c4e15c797293f30c900adfb7a27
Thomas Guettler <info.git@thomas-guettler.de>
2017-03-12 10:45:18 +0100

    storing and loading from mail on server works

--- ndayslater/tests/test_ndayslater.py
+++ ndayslater/tests/test_ndayslater.py
@@ -1,5 +1,6 @@
 import unittest
-from ndayslater.ndayslater import get_config_parser, run
+from ndayslater.ndayslater import get_config_parser, run, NDaysLaterIMAPClient
+
 
 class TestCase(unittest.TestCase):
 
@@ -7,3 +8,7 @@ class TestCase(unittest.TestCase):
         config_parser=get_config_parser()
         args=config_parser.parse_args(['-o', 'localhost', '-u', 'imaptest', '-p', 'imaptest', '--no-ssl', '-v'])
         run(args)
+
+    def test_pycharm_completition(self):
+        client=NDaysLaterIMAPClient()
+        run=client
